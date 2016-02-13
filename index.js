de1cb4392df0fe53477bfde628a162c626e52fe6
Brad Dougherty <me@brad.is>
2016-02-13 17:39:30 -0500

    Update for eslint 2.0

--- index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-var fs = require('fs');
-var path = require('path');
-var yaml = require('js-yaml');
-var stripComments = require('strip-json-comments');
-
-var file = fs.readFileSync(path.join(__dirname, '.eslintrc'), 'utf8');
-module.exports = yaml.safeLoad(stripComments(file));
