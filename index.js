04053e6d23dd9703418dfeafdaf3a489de81ec31
Brad Dougherty <me@brad.is>
2017-12-10 00:48:43 -0500

    Add a couple rule overrides

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 module.exports = {
 	rules: {
 		'arrow-parens': ['error', 'always'],
+		'brace-style': ['error', 'stroustrup'],
+		'capitalized-comments': 'warning',
+		'generator-star-spacing': ['error', 'after'],
 		'object-curly-spacing': ['error', 'always']
 	}
 };
